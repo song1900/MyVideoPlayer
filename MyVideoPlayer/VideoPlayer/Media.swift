a49f267390237a0343c1f3fd820ce6a8114292d6
song1900 <woojin1900@gmail.com>
2023-11-29 22:28:42 +0900

    Feat: Player View, Core 구현

---
+++ MyVideoPlayer/VideoPlayer/Media.swift
@@ -0,0 +1,12 @@
+//
+//  Media.swift
+//  MyVideoPlayer
+//
+//  Created by denver on 2023/11/29.
+//
+
+import Foundation
+
+public struct Media {
+  let url: String
+}
---
+++ MyVideoPlayer/VideoPlayer/PipStatus.swift
@@ -0,0 +1,16 @@
+//
+//  PipStatus.swift
+//  MyVideoPlayer
+//
+//  Created by denver on 2023/11/29.
+//
+
+import Foundation
+
+public enum PipStatus: String {
+  case willStart
+  case didStart
+  case willStop
+  case didStop
+  case unowned
+}
