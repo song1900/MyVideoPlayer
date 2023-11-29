bd4257aa254ac7b9d7c8df3bd828e9ff0424529a
denver <denver@jejememe.com>
2023-11-29 22:19:51 +0900

    Initial Commit

---
+++ MyVideoPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyVideoPlayer
+//
+//  Created by denver on 2023/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyVideoPlayer/MyVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVideoPlayerApp.swift
+//  MyVideoPlayer
+//
+//  Created by denver on 2023/11/29.
+//
+
+import SwiftUI
+
+@main
+struct MyVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
